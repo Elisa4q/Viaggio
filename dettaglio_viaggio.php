d0e723e0bf05f0626b6888d6b696c22058de7873
Elisa4q <145005271+Elisa4q@users.noreply.github.com>
2023-12-09 18:24:33 +0100

    Create dettaglio_viaggio.php

---
+++ dettaglio_viaggio.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>dettaglio prenotazione</title>
+    </head>
+    <body> 
+        <h1>prenotazione</h1>
+        <?php
+            session_start();
+            $nome = $_POST["nome"]; //_POST è un array associativo predefinito in PHP
+            $cognome = $_POST["cognome"]; 
+            $tipo_viaggio = $_POST["tipo_viaggio"];
+
+            $_SESSION["nome"] = $nome; //session è un'array associativo
+            $_SESSION["cognome"] = $cognome;
+            $_SESSION["tipo_viaggio"] = $tipo_viaggio;
+
+        if($tipo_viaggio == "Lavoro"){//Lavoro
+            print("Il tuo viaggio è per: $tipo_viaggio"); 
+        } else {
+            print(""Il tuo viaggio è per: $tipo_viaggio");
+        }
+        
+        ?>
+
+
+        <form method = "post" action = "riepilogo_viaggio.html">
+            Scegli la tua destinazione:  
+            <select name  = "destinazione">
+                <option value = "Roma">Roma</option>
+                <option value = "Tokyo">Tokyo</option>
+                <option value = "Londra">Londra</option>
+                <option value = "New York">New York</option>
+                <option value = "Francia">Francia</option>
+                <option value = "Svizzera">Svizzera</option>
+                <option value = "Parigi">Parigi</option>
+                <option value = "Dubai">Dubai</option>
+                
+            </select>
+            <input type = "submit">
+    </form>
+    </body>
+</html>
