427ab88a0fa5f768e9c068b52a0e0f9c5670a5f5
Elisa4q <elisa.santomauro@iismajoranamoncalieri.edu.it>
2023-12-11 10:56:59 +0100

    modificato

--- riepilogo_viaggio.php
+++ riepilogo_viaggio.php
@@ -12,12 +12,12 @@
             $cognome = $_SESSION["cognome"];
             $nome = $_SESSION["nome"];
             $destinazione = $_POST["destinazione"]; 
-            
+
         
 
             print("Buongiorno $nome $cognome siamo lieti di comunicarti che la tua destinazione è : $destinazione");
 
-           
+        
          ?>
 
 
