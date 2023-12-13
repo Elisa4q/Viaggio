f8fe6d944eb8ea61f064a885a8ff400b751f2ebf
Elisa4q <145005271+Elisa4q@users.noreply.github.com>
2023-12-13 14:12:34 +0000

    modifiche

--- riepilogo_viaggio.php
+++ riepilogo_viaggio.php
@@ -9,16 +9,31 @@
         <?php
             session_start();
 
-            $Cognome = $_SESSION["Cognome"];
-            $Nome = $_SESSION["Nome"];
-            $destinazione = $_SESSION["destinazione"]; 
-            $budget = $_SESSION["budget"];
-            $hotel = $_SESSION["hotel"];
-            $azienda = $_SESSION["azienda"];
-            $durata_viaggio = $_SESSION["durata_viaggio"]; 
-            $num_persone = $_SESSION["num_persone"];
-            $tipo_alloggio = $_SESSION["tipo_alloggio"];
+            session_start();
+            $Nome = $_POST["Nome"]; //_POST è un array associativo predefinito in PHP
+            $Cognome = $_POST["Cognome"]; 
+            $Tipo_viaggio = $_POST["Tipo_viaggio"];
+            $destinazione = $_POST["destinazione"] ;
+            $budget = $_POST["budget"];
+            $hotel = $_POST["hotel"];
+            $azienda = $_POST["azienda"];
+            $durata_viaggio = $_POST["durata_viaggio"];
+            $num_persone = $_POST["num_persone"]; 
+            $tipo_alloggio = $_POST["tipo_alloggio"];
             $attività = $_SESSION["attività"];
+
+            $_SESSION["Nome"] = $Nome; //session è un'array associativo
+            $_SESSION["Cognome"] = $Cognome;
+            $_SESSION["Tipo_viaggio"] = $Tipo_viaggio;
+            $_SESSION["destinazione"] = $destinazione;
+            $_SESSION["budget"] = $budget;
+            $_SESSION["hotel"] = $hotel;
+            $_SESSION["azienda"] = azienda;
+            $_SESSION["durata_viaggio"] = $durata_viaggio;
+            $_SESSION["num_persone"] = $num_persone; 
+            $_SESSION["tipo_alloggio"] = $tipo_alloggio;
+            $_SESSION["attività"] = $attività;
+            
             ?>
     </body>
 </html>
