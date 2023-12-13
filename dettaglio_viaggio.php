ae1d8701195b5bd70371fa55258815901695f8f7
Elisa4q <145005271+Elisa4q@users.noreply.github.com>
2023-12-13 14:09:04 +0000

    MODIFICHE

--- dettaglio_viaggio.php
+++ dettaglio_viaggio.php
@@ -11,11 +11,30 @@
             $Nome = $_POST["Nome"]; //_POST è un array associativo predefinito in PHP
             $Cognome = $_POST["Cognome"]; 
             $Tipo_viaggio = $_POST["Tipo_viaggio"];
-           
+            $destinazione = $_POST["destinazione"] ;
+            $budget = $_POST["budget"];
+            $hotel = $_POST["hotel"];
+            $azienda = $_POST["azienda"];
+            $durata_viaggio = $_POST["durata_viaggio"];
+            $num_persone = $_POST["num_persone"]; 
+            $tipo_alloggio = $_POST["tipo_alloggio"];
+            $attività = $_SESSION["attività"];
 
             $_SESSION["Nome"] = $Nome; //session è un'array associativo
             $_SESSION["Cognome"] = $Cognome;
             $_SESSION["Tipo_viaggio"] = $Tipo_viaggio;
+            $_SESSION["destinazione"] = $destinazione;
+            $_SESSION["budget"] = $budget;
+            $_SESSION["hotel"] = $hotel;
+            $_SESSION["azienda"] = azienda;
+            $_SESSION["durata_viaggio"] = $durata_viaggio;
+            $_SESSION["num_persone"] = $num_persone; 
+            $_SESSION["tipo_alloggio"] = $tipo_alloggio;
+            $_SESSION["attività"] = $attività;
+
+
+
+
 
         if($Tipo_viaggio == "L"){//Lavoro
             print("Il tuo viaggio è per: $Tipo_viaggio");
@@ -23,6 +42,8 @@
         } else { //Vacanza
             print("Il tuo viaggio è per: $Tipo_viaggio");
         };
+
+
 
         if($Tipo_viaggio == "L") { //lavoro
             
@@ -67,9 +88,6 @@
             print("<option value = '1 stella'>1 stella</option>");
             print("<option value = 'b&b'>b&b</option>");
             print("<option value = 'Appartamento'>Appartamento</option>");
-
-
-
         };
 
         ?>
