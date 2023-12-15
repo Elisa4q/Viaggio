1c0ec480314f2831bc59b7657ec48d3a40e1a794
Elisa4q <elisa.santomauro@iismajoranamoncalieri.edu.it>
2023-12-15 10:46:41 +0100

    mdoifiche

--- riepilogo_viaggio.php
+++ riepilogo_viaggio.php
@@ -7,21 +7,9 @@
     <body> 
         <h1>Riepilogo</h1> 
         <?php
-            session_start();
 
             session_start();
-            $Nome = $_POST["Nome"]; //_POST è un array associativo predefinito in PHP
-            $Cognome = $_POST["Cognome"]; 
-            $Tipo_viaggio = $_POST["Tipo_viaggio"];
-            $destinazione = $_POST["destinazione"] ;
-            $budget = $_POST["budget"];
-            $hotel = $_POST["hotel"];
-            $azienda = $_POST["azienda"];
-            $durata_viaggio = $_POST["durata_viaggio"];
-            $num_persone = $_POST["num_persone"]; 
-            $tipo_alloggio = $_POST["tipo_alloggio"];
-            $attività = $_POST["attività"];
-
+            
             $_SESSION["Nome"] = $Nome; //session è un'array associativo
             $_SESSION["Cognome"] = $Cognome;
             $_SESSION["Tipo_viaggio"] = $Tipo_viaggio;
