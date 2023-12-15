75612e4acce5847036840c86137a7cfa6062ec59
Elisa4q <elisa.santomauro@iismajoranamoncalieri.edu.it>
2023-12-15 10:24:31 +0100

    modifiche

--- riepilogo_viaggio.php
+++ riepilogo_viaggio.php
@@ -9,17 +9,17 @@
         <?php
             session_start();
 
-            $Cognome = $_SESSION["Cognome"];
-            $Nome = $_SESSION["Nome"];
-            $Tipo_viaggio = $_POST["Tipo_viaggio"];
-            $destinazione = $_SESSION["destinazione"]; 
-            $budget = $_SESSION["budget"];
-            $hotel = $_SESSION["hotel"];
-            $azienda = $_SESSION["azienda"];
-            $durata_viaggio = $_SESSION["durata_viaggio"]; 
-            $num_persone = $_SESSION["num_persone"];
-            $tipo_alloggio = $_SESSION["tipo_alloggio"];
-            $attività = $_SESSION["attività"];
+            $Cognome = $_COOKIE["Cognome"];
+            $Nome = $_COOKIE["Nome"];
+            $Tipo_viaggio = $_COOKIE["Tipo_viaggio"];
+            $destinazione = $_COOKIE["destinazione"]; 
+            $budget = $_COOKIE["budget"];
+            $hotel = $_COOKIE["hotel"];
+            $azienda = $_COOKIE["azienda"];
+            $durata_viaggio = $_COOKIE["durata_viaggio"]; 
+            $num_persone = $_COOKIE["num_persone"];
+            $tipo_alloggio = $_COOKIE["tipo_alloggio"];
+            $attività = $_COOKIE["attività"];
            
         print("Buongiorno $Nome $Cognome siamo lieti di informala che la sua destinazione è: $destinazione
         lei ha un budget: $budget, l'hotel selezionato è un : $hotel");
