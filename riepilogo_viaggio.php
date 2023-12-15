c3c61cca20f882f117888ef858d713df4b4c8ae9
Elisa4q <145005271+Elisa4q@users.noreply.github.com>
2023-12-15 17:10:52 +0100

    Update riepilogo_viaggio.php

--- riepilogo_viaggio.php
+++ riepilogo_viaggio.php
@@ -21,7 +21,21 @@
             $num_persone = $_POST["num_persone"]; 
             $tipo_alloggio = $_POST["tipo_alloggio"];
             $attività = $_POST["attività"]; 
-            
+
+
+if($tipo_viaggio == "lavoro"){
+print("Benvenuto $Nome $Cognome, siamo leiti di informarla che il suo viaggio è per $tipo_viaggio
+    la destinazione da lei selezionata è $destinazione
+    la sua azienda è $azienda, lei ha un budget di $budget,
+    il suo viaggio dura $durata_viaggio");
+    } else {
+    print("Benvenuto $Nome $cognome, siamo leiti di infomarla che il suo viaggio è per $tipo_viaggio
+    la destinazione da lei selezionata è $destinazione, 
+    siete in $num_persone persone, 
+    come alloggio avete selezionato un $tipo_aloggio, infine come attività interessante 
+    avete inserito $attività interessante");
+    };
+
             ?>
     </body>
 </html>
